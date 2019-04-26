6a2d24dbfe5ca73317fc51c9efea4a93e8e03e68
Brian Giannini <brian.giannini.1@gmail.com>
2019-04-26 15:00:27 +0200

    field injection

--- app/src/main/java/io/train/dagger2training/MainActivity.kt
+++ app/src/main/java/io/train/dagger2training/MainActivity.kt
@@ -2,18 +2,20 @@ package io.train.dagger2training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private var car: Car? = null
+    @set:Inject
+    var car: Car? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val component: CarComponent = DaggerCarComponent.create()
+        component.inject(this)
 
-        car = component.getCar()
         car?.drive()
     }
 }
